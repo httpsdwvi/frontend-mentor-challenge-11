7c82b88f3f6258497d448491fbab4c03a62b7582
httpsdwvi <davi.santos123@etec.sp.gov.br>
2023-02-27 21:56:54 -0300

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
 console.log("Working ...")
 
+//Color Palette
+const NOTSEEN = "hsl(219, 14%, 63%)"
+const DOTNOTSEEN = "hsl(1, 90%, 64%)"
+const SEEN = "hsl(0, 0%, 100%)"
+
 function resetCount() {
-    document.getElementById("notificationNumber").innerHTML = "0"
+    document.querySelector(".notification-number").innerHTML = "0"
+
 }
